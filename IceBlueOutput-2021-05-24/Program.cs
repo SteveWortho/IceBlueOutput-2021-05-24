4bddd0ab82afde799df9a27e9d3f0014f43b95ed
Steve Worthington (Podium) <steve@podiumsoftware.net>
2021-05-25 13:00:51 +1200

    Add more example docx and outputs.

--- IceBlueOutput-2021-05-24/Program.cs
+++ IceBlueOutput-2021-05-24/Program.cs
@@ -16,7 +16,19 @@ namespace IceBlueOutput_2021_05_24
 
         static void Main(string[] args)
         {
+            // Issue 01
+            // When converting List Paragrapg from this document, the Indentation 
+            // is not preserved.
+            ConvertToPdf("CohortReport-417b95f2-8c4c-46c9-9b50-f6060c9f89ba.docx");
+            // Issue 01-a
+            // But this document, which appear to be the same converts correctly
+            // What is it that Spire PDF Writer is interpretting different please?
+            ConvertToPdf("CohortReport.docx");
+
+            // Issue 02 - This image has a large graphic in it, it stretches and distorts
             ConvertToPdf("TestBigGraphic.docx");
+            
+           
         }
 
         public static void ConvertToPdf(string sourceDocx)
